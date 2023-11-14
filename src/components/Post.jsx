c35a567cb8054940d8dfb0daab0a66ddb3597660
Philip Joseph <139831746+phlptnc@users.noreply.github.com>
2023-11-14 17:03:16 +0800

    react slick

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,39 @@
 import React from "react"
 import SectionHeader from "./sectionHeader"
+import Slider from "react-slick"
+import PrevArrow from "./Buttons/PrevArrow"
+import NextArrow from "./Buttons/NextArrow"
+
+var settings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    draggable: true,
+    speed: 500,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+            },
+        },
+        {
+            breakpoint: 760,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+    ],
+}
 
 function Post() {
     const postData = [
@@ -21,6 +55,12 @@ function Post() {
             description:
                 "This is program designed to make the practice of yoga beneficial for people of all ages, abilities, and backgrounds.",
         },
+        {
+            date: "14 Nov 2023",
+            title: "Yoga for Everyone 2023",
+            description:
+                "This is program designed to make the practice of yoga beneficial for people of all ages, abilities, and backgrounds.",
+        },
     ]
 
     return (
@@ -28,13 +68,17 @@ function Post() {
             <section className="bg-[#f5f5f5]">
                 <div className="container py-[3rem] min-[400px]:py-[6rem] px-10 lg:px-0 ">
                     <SectionHeader title="Latest Posts" />
-                    <div className="flex justify-between gap-6">
+
+                    <Slider {...settings}>
                         {postData.map((item) => (
-                            <div className="blog md:w-1/3 flex flex-col gap-3">
+                            // Card
+                            <div className="blog-card">
                                 {/* image */}
                                 <div className="relative">
-                                    <div className="bg-[#fb5b21] text-center text-[#f5f5f5] font-semibold font-sans leading-5 py-3 px-3 absolute left-5 top-[-1rem] z-20">
-                                        <p className="w-[40px]">{item.date}</p>
+                                    <div className="bg-[#fb5b21] text-center text-[#f5f5f5] font-semibold font-sans leading-5 min-[400px]:py-3 py-1 min-[400px]:px-3 px-1 absolute min-[400px]:left-5 left-3 top-[-1rem] z-20">
+                                        <p className="w-[40px] text-[0.8rem] min-[400px]:text-[1rem]">
+                                            {item.date}
+                                        </p>
                                     </div>
                                     <div className="overflow-hidden">
                                         <img
@@ -44,12 +88,12 @@ function Post() {
                                                 ".jpg"
                                             }
                                             alt={item.title}
-                                            className="h-[150px] min-[400px]:h-[250px] w-full hover:scale-110 ease-in-out duration-700 cursor-pointer"
+                                            className="h-[200px] min-[400px]:h-[250px] w-full hover:scale-110 ease-in-out duration-700 cursor-pointer object-cover"
                                         />
                                     </div>
                                 </div>
                                 {/* lower section */}
-                                <h3 className="text-[1.5rem] min-[400px]:text-[2rem] font-semibold hover:text-[#fb5b21] hover:underline cursor-pointer ease-in-out duration-300">
+                                <h3 className="text-[1.2rem] min-[400px]:text-[1.5rem] font-semibold hover:text-[#fb5b21] hover:underline cursor-pointer ease-in-out duration-300">
                                     {item.title}
                                 </h3>
                                 <div>
@@ -57,7 +101,8 @@ function Post() {
                                 </div>
                             </div>
                         ))}
-                    </div>
+                    </Slider>
+                    <div className="flex justify-between gap-6"></div>
                 </div>
             </section>
         </>
